a14ffefa6260bf5538305b1f4e49b9ac48b1cf92
Giovanni Puntil <puntilgiovanni@gmail.com>
2020-07-16 19:08:38 +0100

    Bootstrap

---
+++ src/components/newsletter.js
@@ -0,0 +1,79 @@
+import React, { Component } from "react"
+import jsonp from "jsonp"
+
+import styles from "./newsletter.module.css"
+import globalStyles from "../pages/index.module.css"
+
+const DOMAIN_ID = "daam"
+const USER_ID = "77dd2a5a3bf460239601116ed"
+const CAMPAIGN_ID = "559373b904"
+const MAIL_CHIMP_API_URL = `https://${DOMAIN_ID}.us10.list-manage.com/subscribe/post-json?u=${USER_ID}&amp;id=${CAMPAIGN_ID}`
+
+class Newsletter extends Component {
+  state = {
+    emailAddress: "",
+    status: ""
+  }
+
+  handleSubmit = async event => {
+    event.preventDefault();
+
+    const url = `${MAIL_CHIMP_API_URL}&EMAIL=${this.state.emailAddress}`
+
+    this.setState({
+      status: "sending"
+    })
+
+    jsonp(
+      url,
+      {
+        param: "c"
+      },
+      (err, data) => {
+        if (err) {
+          this.setState({
+            status: "error",
+            message: err
+          })
+        } else if (data.result !== "success") {
+          this.setState({
+            status: "error",
+            message: data.msg
+          })
+        } else {
+          this.setState({
+            status: "success",
+            message: data.msg,
+            emailAddress: ""
+          })
+        }
+      }
+    )
+  }
+
+  handleChange = event => {
+    this.setState({ emailAddress: event.target.value });
+  }
+
+  render() {
+    return (
+      (
+        <div className={styles.container}>
+          <form onSubmit={this.handleSubmit} className={styles.form}>
+                <input type="email" value={this.state.emailAddress} onChange={this.handleChange} name="EMAIL" className={styles.emailAddress} placeholder="mail" required />
+                <div className={styles.hiddenField} aria-hidden="true">
+                  <input type="text" name="b_77dd2a5a3bf460239601116ed_559373b904" tabIndex="-1" value="" readOnly />
+                </div>
+                <input type="submit" value="VAI" name="subscribe" className={styles.submitButton} />
+          </form>
+          {this.state.status === "sending" && <div className={styles.inProgressMessage}>Iscrizione in corso...</div>}
+          {this.state.status === "error" && <div className={styles.errorMessage} id="newsletter-error" dangerouslySetInnerHTML={{__html: this.state.message}}/>}
+          {this.state.status === "success" && <div className={styles.successMessage}>Tutto fatto, Grazie! <span role="img">👍</span></div>}
+          <div><a href="https://www.iubenda.com/privacy-policy/38606123" rel="noreferrer" target="_blank" className={[styles.privacy, globalStyles.link].join(" ")}>Privacy</a></div>
+        </div>
+      )
+    )
+  }
+}
+
+export default Newsletter
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,59 @@
 import React from "react"
+import { Link } from "gatsby"
+import Seo from "../components/seo"
+import Newsletter from "../components/newsletter"
 
-export default function Home() {
-  return <div>Hello world!</div>
-}
+import logo from "./logo.svg"
+import styles from "./index.module.css"
+
+const Home = () => (
+  <div>
+    <Seo/>
+    <section className={styles.section}>
+      <main className={styles.content}>
+        <div className={styles.head}>
+          <div>
+            <img className={styles.logo} src={logo} alt="Dovevo andare al mia"/>
+            <div className={styles.logoSub}>(dovevo andare al mia)</div>
+          </div>
+
+          <div className={styles.date}>
+            <div>1-4</div>
+            <div className={styles.subDate}>ottobre</div>
+          </div>
+        </div>
+
+        <div className={styles.mainTextBig}>Fuorieventi milanesi della fiera che non c'è*</div>
+        </main>
+    </section>
+    <section className={styles.section}>
+      <main className={styles.content}>
+        <div className={styles.head}>
+          <div>
+            <h2 className={styles.subtitle}>il DAAM?</h2>
+          </div>
+        </div>
+
+        <div className={styles.mainTextMedium}>* Mentre aspettiamo che il MIA riparta <u>(la decima edizione si terrà dal 25 - 28 marzo)</u>, arriva il DAAM! Un’ iniziativa per colmare il vuoto di questo 2020 atipico, un hub per promuovere la fotografia d’autore. Quattro giorni ricchi di eventi e progetti fotografici di grandi maestri organizzati dalle più grandi e prestigiose gallerie fotografiche milanesi.</div>
+        </main>
+    </section>
+    <section className={[styles.section, styles.greenBg].join(' ')}>
+      <main className={styles.content}>
+        <div className={styles.head}>
+          <div>
+            <h2 className={styles.subtitle}>Eventi</h2>
+          </div>
+        </div>
+
+        <div className={styles.eventiText}>
+          dal 25 luglio pubblicheremo la lista degli eventi, torna a trovarci!
+          <br/><br/>
+          intanto iscriviti alla newsletter per rimanere sempre aggiornato
+        </div>
+        <Newsletter/>
+      </main>
+    </section>
+  </div>
+)
+
+export default Home
