57daaf9dd0a34c85a03bd84a7aa502df42fe248d
Giovanni Puntil <puntilgiovanni@gmail.com>
2020-07-24 15:23:07 +0100

    Update labels

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,7 +6,7 @@ import logo from "../images/mia-logo.jpg"
 
 const Footer = () => (<footer className={styles.footer}>
     <div className={styles.content}>
-      DAAM è un’iniziativa con il patrocinio del <a href="https://www.miafair.it/">MIA Photo Fair</a>.
+      DAAM è un’iniziativa con il patrocinio del <a href="https://www.miafair.it/">MIA (Milan Image Art) Fair</a>.
 
       <div className={styles.notice}>Tutti i diritti riservati © {new Date().getFullYear()}</div>
 
