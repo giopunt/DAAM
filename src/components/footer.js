d4f4ba7555f59e1759094f314ff76feb079b5eae
Giovanni Puntil <puntilgiovanni@gmail.com>
2020-09-23 23:18:23 +0100

    Added press release

--- src/components/footer.js
+++ src/components/footer.js
@@ -10,6 +10,7 @@ const Footer = () => (<footer className={styles.footer}>
       <p><a href="https://www.miafair.it/">MIA Fair</a> è la prima e più importante Fiera d’arte dedicata alla fotografia e all’immagine in movimento in Italia, nata con l’obiettivo di evidenziare il ruolo trasversale che questo mezzo ha assunto tra i linguaggi espressivi dell’arte e del sistema dell’arte contemporanea</p>
       <a href="https://www.miafair.it/" className={styles.miaLogo}><img src={logo} alt="MIA logo" /></a>
       <div className={styles.notice}>Tutti i diritti riservati © {new Date().getFullYear()}</div>
+      <div className={styles.press}><a href="/DAAM_cs.pdf">Scarica qui</a> il comunicato stampa per maggiori informazioni scrivere a: <a href="mailto:confalonieri.eleonora@gmail.com">confalonieri.eleonora@gmail.com</a></div>
     </div>
   </footer>
 )
